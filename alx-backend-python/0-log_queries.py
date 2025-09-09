1c4096ac1481c9fab03c0d810a0183b6399c0ef7
dev-Bambam <ayogood18@gmail.com>
2025-09-09 14:21:56 +0100

    I added a docstring

--- alx-backend-python/0-log_queries.py
+++ alx-backend-python/0-log_queries.py
@@ -1,3 +1,7 @@
+'''
+Ok so I just learnt about decorator in python and it's really great, it's a great way of modifying the behavior of a function without changing the function's functionality itself. a decorator is a function that takes a function as an arg and return the function, the function the decorator take as an argument is the function/methods we want to decorate, then the decorator adds a wrapper function around the function which will have acess to the args of the function/methods that needs to be decorated, we return the decorator arg from the wrapper func so that the function can run and we return the wrapper as a variable.
+'''
+
 import sqlite3
 
 
@@ -5,10 +9,10 @@ import sqlite3
 # decorator to log sql queries
 def log_queries(func):
     def wrapper(query):
-        print(f'query:{query}')
+        print(f'query: {query}')
         return func(query)
     
-    return wrapper()
+    return wrapper
 
 @log_queries
 def fetch_all_users(query):
@@ -21,5 +25,3 @@ def fetch_all_users(query):
     return result
 
 users = fetch_all_users('SELECT * FROM users')
-
-    
