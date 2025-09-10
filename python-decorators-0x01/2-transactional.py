5e2f07860826292cf4fc653150d2c0ef5c58e6fb
dev-Bambam <ayogood18@gmail.com>
2025-09-10 17:48:49 +0100

    ok the code have been revamped

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -1,32 +1,31 @@
 import sqlite3
 import os
 
+
 def with_db_connection(func):
     def wrapper(*args, **kwargs):
-        db_path = os.path.join(os.getcwd(), 'db', 'user.db')
-        try:
-            with sqlite3.connect(db_path) as conn:
-                args = (conn,) + args
-                result = func(*args, **kwargs)
-        except sqlite3.Error as e:
-            return e
-        return result
-    
+        db_path = os.path.join(os.getcwd(), "db", "user.db")
+        with sqlite3.connect(db_path) as conn:
+            args = (conn,) + args
+            return func( *args, **kwargs)
+        
+
     return wrapper
+
 
 def transactional(func):
-    def wrapper(*args, **kwargs):
-        conn = args[0]
+    def wrapper(conn, *args, **kwargs):
         try:
-             func(*args, **kwargs)
-        except Exception as e:
+            result = func(conn, *args, **kwargs)
+        except sqlite3.Error as e:
             conn.rollback()
-            print(f'Error Occured during transaction:{e}')
+            print(f"Error Occured during transaction:{e}")
+            raise
         else:
             conn.commit()
+            return result
 
     return wrapper
-
 
 
 @with_db_connection
@@ -35,6 +34,6 @@ def update_user_email(conn, user_id, new_email):
     cursor = conn.cursor()
     cursor.execute("UPDATE users SET email=? WHERE id=?", (new_email, user_id))
 
-result = update_user_email(user_id=1, new_email='ay@gmail.com')
-print(f'result coming from the last print: {(result)}')
-    
+
+result = update_user_email(user_id=1, new_email="ay@gmail.com")
+print(f"result coming from the last print: {(result)}")
