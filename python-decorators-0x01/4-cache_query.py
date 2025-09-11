d1e492e3ffbdd0124b555296adfb594ff69d8961
dev-Bambam <ayogood18@gmail.com>
2025-09-11 16:56:34 +0100

    I learnt alot about caching db queries and implementing a robust decorator that works accross diff function that sends a query to the db

--- python-decorators-0x01/4-cache_query.py
+++ python-decorators-0x01/4-cache_query.py
@@ -21,18 +21,38 @@ def with_db_connection(func):
     return wrapper
 
 
+# my initial implementation
+
+# def cache_query(func):
+#     cache = {}
+#     def wrapper(*args, **kwargs):
+#         cache_key  = kwargs['query']
+#         if cache_key in cache:
+#             return cache[cache_key]
+#         else:
+#             result = func(*args, **kwargs)
+#             cache[cache_key] = result
+
+#         return result
+        
+#     return wrapper
+
 def cache_query(func):
-    cache = {}
+    cache = {} # this create a global cache for all function
+    cache.setdefault(func.__name__, {}) # this set each unique func call as a key which the value is a {} which contain the different args and kwargs of the func as the key and the result of the function call
+
     def wrapper(*args, **kwargs):
-        cache_key  = kwargs['query']
+        query_args = args[1:]  # line 45 and 46 makes both args/kwargs a unique key by converting them to tuples and sorting the kwargs so that the order of the kwargs doesn't matter so far they are the same
+        cache_key = (query_args, tuple(sorted(kwargs.items())))
+
         if cache_key in cache:
             return cache[cache_key]
         else:
             result = func(*args, **kwargs)
             cache[cache_key] = result
-
-        return result
         
+        return result
+    
     return wrapper
 
 @with_db_connection
