23169d18d864998fbf414ae86e88c1171bb6eac7
dev-Bambam <ayogood18@gmail.com>
2025-09-16 23:17:59 +0100

    ..

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,28 +8,28 @@ from parameterized import parameterized
 import requests
 
 
-class TestAccessNestedMap(TestCase):
-    ''' A test class for the util module'''
+# class TestAccessNestedMap(TestCase):
+#     ''' A test class for the util module'''
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    def test_access_nested_map(self, nested_map, path, result):
-        ''' Test that test the nested map and check if it returns the valid output with a valid input'''
+#     @parameterized.expand([
+#         ({"a": 1}, ("a",), 1),
+#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
+#         ({"a": {"b": 2}}, ("a", "b"), 2),
+#     ])
+#     def test_access_nested_map(self, nested_map, path, result):
+#         ''' Test that test the nested map and check if it returns the valid output with a valid input'''
 
-        self.assertEqual(access_nested_map(nested_map, path), result)
+#         self.assertEqual(access_nested_map(nested_map, path), result)
 
-    @parameterized.expand([
-        ({},('a', ), KeyError),
-        ({'a':1}, ('a','b'), KeyError)
-    ])
-    def test_access_nested_map_exception(self, nested_map, path, error):
-        ''' A Test that check if exception is raised for wrong input '''
+#     @parameterized.expand([
+#         ({},('a', ), KeyError),
+#         ({'a':1}, ('a','b'), KeyError)
+#     ])
+#     def test_access_nested_map_exception(self, nested_map, path, error):
+#         ''' A Test that check if exception is raised for wrong input '''
         
-        with self.assertRaises(error):
-            access_nested_map(nested_map, path)
+#         with self.assertRaises(error):
+#             access_nested_map(nested_map, path)
                     
 
 class TestGetJson(TestCase):
