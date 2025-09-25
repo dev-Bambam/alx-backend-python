1debfdec305c1c636ccecd2ead80763e05018af5
Bambam <ayogood18@gmail.com>
2025-09-25 18:14:32 +0100

    update

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class isAuthenticatedAndOwner(BasePermission):
+class isAuthenticatedAndOwner(permissions.BasePermission):
     '''Custom permission to only allow owners of an object to view or edit it'''
 
     def has_object_permission(self, request, view, obj):
