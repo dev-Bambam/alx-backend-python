49e7ac4d2d0fda11d1b4e56227b8e8458e53cdc6
Bambam <ayogood18@gmail.com>
2025-09-25 18:12:36 +0100

    updated

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+class isAuthenticatedAndOwner(BasePermission):
+    '''Custom permission to only allow owners of an object to view or edit it'''
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any authenticated user,
+        # but write permissions are only allowed to the owner of the project
+        return request.user and obj.user == request.user
