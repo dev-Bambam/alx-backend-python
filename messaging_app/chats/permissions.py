02107eba56d767ae64b8430630428d9a0357e0d4
Bambam <ayogood18@gmail.com>
2025-09-26 05:43:36 +0100

    task 2 done

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -7,3 +7,21 @@ class isAuthenticatedAndOwner(permissions.BasePermission):
         # Read permissions are allowed to any authenticated user,
         # but write permissions are only allowed to the owner of the project
         return request.user and obj.user == request.user
+    
+class isParticipantOfConversation(permissions.BasePermission):
+    '''Custom permission to allow a participant in a conversation to access it'''
+
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is one of the participants in the conversation.
+        # This assumes your Chat model has a 'participants' field.
+        # For a simple user model, this means checking if the user is the owner.
+        # For a more complex model, you'd check a ManyToManyField.
+        # For this task, we will assume the conversation is linked to a user via ForeignKey
+        # and they are therefore a participant.
+
+        if hasattr(obj, 'chat'):
+            chat = obj.chat
+        else:
+            chat = obj
+
+        return chat.user == request.user or request.user in chat.participants.all() if hasattr(chat, 'participant') else chat.user == request.user
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,2 +1,31 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from .models import Chat, Message
+from .permissions import isParticipantOfConversation
+from .serializers import ChatSerializer, MessageSerializer
+
+class ChatViewSet(viewsets.ModelViewSet):
+    serializer_class = ChatSerializer
+    permission_classes = [IsAuthenticated, isParticipantOfConversation]
+
+    def get_queryset(self):
+        return Chat.objects.filter(user=self.request.user).order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
+    
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, isParticipantOfConversation]
+
+    def get_queryset(self):
+        return Message.objects.filter(chat__user=self.request.user).order_by('timestamp')
+    
+    def perform_create(self, serializer):
+        chat_id = self.kwargs.get('chat_pk')
+        chat = Chat.objects.get(pk=chat_id)
+        if chat.user != self.request.user:
+            raise PermissionDenied('You can only send messages to your own chats')
+        serializer.save(sender=self.request.user, chat=chat)
