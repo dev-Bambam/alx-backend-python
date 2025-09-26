1bc9d06f90dd2b341765c93f0eaa0fa53a000d7c
Bambam <ayogood18@gmail.com>
2025-09-26 16:56:11 +0100

    added postman collections

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Chat(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200,default='New Chat' )
+    participants = models.ManyToManyField(User, on_delete=models.CASCADE, related_name='chats')
+    title = models.CharField(max_length=200,default='New Chat', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,50 +1,56 @@
-from rest_framework import viewsets
+from django.db.models import Q
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
-from .pagination import StandardResultsSetPagination
-from filters import MessageFilter
 from rest_framework.response import Response
+from serializers import ChatSerializer, MessageSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status
 from .models import Chat, Message
 from .permissions import isParticipantOfConversation
-from .serializers import ChatSerializer, MessageSerializer
+from pagination import StandardResultsSetPagination
+from filters import MessageFilter
 
 class ChatViewSet(viewsets.ModelViewSet):
     serializer_class = ChatSerializer
-    permission_classes = [IsAuthenticated, isParticipantOfConversation]
+    permission_classes = [isParticipantOfConversation]
+
+    def get_queryset(self):
+        """Filters the queryset to filter chat where requesting user is participants of chat"""
+        user = self.request.user
+        return Chat.objects.filter(Q(participants=user)).order_by("-created_at")
+
+    def perform_create(self, serializer):
+        '''Automatically adds the authenticated user as a participant a when creating a new chat'''
+        chat = serializer.save()
+        chat.participants.add(self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [isParticipantOfConversation]
     pagination_class = StandardResultsSetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_class = MessageFilter
 
     def get_queryset(self):
-        return Chat.objects.filter(user=self.request.user).order_by('-created_at')
+        user = self.request.user
+        return Message.objects.filter(Q(chat__participants = user)).order_by('timestamp')
     
     def perform_create(self, serializer):
-        return serializer.save(user=self.request.user)
-    
-
-class MessageViewSet(viewsets.ModelViewSet):
-    serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated, isParticipantOfConversation]
-
-
-    def get_queryset(self):
-        return Message.objects.filter(chat__user=self.request.user).order_by('timestamp')
-    
-    def perform_create(self, serializer):
+        '''Automatically links the new message to the correct chat and sender'''
         conversation_id = self.kwargs.get('chat_pk')
+
         try:
             chat = Chat.objects.get(pk=conversation_id)
         except Chat.DoesNotExist:
             return Response(
                 {'detail': 'conversation not found'},
-                status=status.HTTP_404_NOT_FOUND 
+                status=status.HTTP_404_NOT_FOUND
             )
         
-        if chat.user != self.request.user:
+        # check if user is a participant of the chat
+        if self.request.user not in chat.participants.all():
             return Response(
-                {'detail': 'You do not have permission to perform this action'},
-                status= status.HTTP_403_FORBIDDEN
+                {'detail':'user not a participant'},
+                status=status.HTTP_403_FORBIDDEN
             )
         
-        serializer.save(sender = self.request.user, chat = chat)
+        serializer.save(sender=self.request.user, chat=chat)
